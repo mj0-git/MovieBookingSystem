261e7dd609802b391c940e2ef8c26cd81fdecad7
“mj0-git” <meraj0.patel@gmail.com>
2022-10-18 19:09:40 -0400

    base

---
+++ src/main/java/org/booking/system/ServletIntializer.java
@@ -0,0 +1,12 @@
+package org.booking.system;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletIntializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources();
+    }
+}
